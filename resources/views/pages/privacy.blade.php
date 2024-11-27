9f8ab4a39469004949de3a10caf5772f27aca7e1
GulnurMirzayeva <gmirzyeva1@gmail.ru>
2024-11-27 21:36:15 +0400

    first commit

---
+++ resources/views/pages/privacy.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('title', 'Privacy Policy')
+
+@section('content')
+    <div class="container terms-conditions">
+        <h2>Privacy Policy</h2>
+        <p>Privacy Policy for Khelbokoi.com</p>
+        <p>At Khelbokoi.com, we are committed to protecting the privacy and personal information of our users. This Privacy Policy outlines how we collect, use, and safeguard the information provided to us through the use of our website.</p>
+        <p>0.</p>
+        <p>0.1 <span>Information Collection</span> 1.1 Personal Information: We may collect personal information, such as name, email address, phone number, and payment details, when users create an account, make a booking, or communicate with us. 1.2 Log Data: Our servers automatically collect certain information, including IP addresses, browser type, referring/exit pages, and operating system, to analyze trends, administer the site information.</p>
+        <p>0.2 <span>Information Use</span> 2.1 Communication: We may use the provided contact details to communicate with users regarding their bookings, updates, promotions, or other relevant information. 2.2 Transaction Processing: We use secure payment gateways to process payments and do not store credit card or other payment information on our servers.</p>
+        <p>0.3 <span>Information Sharing</span> 3.1 Service Providers: We may engage trusted third-party service providers to assist in operating our website and providing services to users. These providers are obligated to maintain the confidentiality and security of the information. 3.2 Legal Compliance: We may disclose personal information if required to do so by law or in response to valid legal requests, such as to comply with a court order or government regulation.</p>
+        <p>0.4 <span>Data Security</span> We employ industry-standard security measures to protect the information provided to us. However, no method of transmission or storage is completely secure. Users acknowledge and accept the inherent risks involved in sharing information online.</p>
+        <p>0.5 <span>Third-Party Links</span> Our website may contain links to third-party websites or services. We are not responsible for the privacy practices or content of such websites. We encourage users to review the privacy policies of these third-party sites.</p>
+        <p>0.6 <span>Children's Privacy</span> Khelbokoi.com is not intended for children under the age of 18. We do not knowingly collect personal information from individuals under 18 years of age. If we become aware that we have collected personal information from a child, we will take steps to delete it.</p>
+        <p>0.7 <span>Changes to Privacy Policy</span> We reserve the right to update or modify this Privacy Policy at any time without prior notice. Users are responsible for reviewing the policy periodically. Continued use of Khelbokoi.com after any changes implies acceptance of the updated Privacy Policy.</p>
+        <p>1. For any questions or concerns regarding our Privacy Policy, please contact us using the information provided on our website.</p>
+
+    </div>
+@endsection
